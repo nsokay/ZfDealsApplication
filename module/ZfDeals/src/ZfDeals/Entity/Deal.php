2c899b74e85d5448188db79a84ac3495a091787a
nsokay <n.smirnovs.0612@gmail.com>
2017-06-27 16:03:46 +0300

    - git ignore

---
+++ module/ZfDeals/src/ZfDeals/Entity/Deal.php
@@ -0,0 +1,88 @@
+<?php
+namespace ZfDeals\Entity;
+
+class Deal
+{
+    protected $id;
+    protected $price;
+    protected $startDate;
+    protected $endDate;
+    protected $product;
+
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+    }
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $startDate
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $product
+     */
+    public function setProduct($product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+}
